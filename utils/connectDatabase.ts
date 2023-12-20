921818d7069e88d6f2f940621e9a429ac65d8562
vishrudh raj <vishrudh.shrinivas@gmail.com>
2023-12-20 22:24:55 +0530

    mongodb write concern

--- utils/connectDatabase.ts
+++ utils/connectDatabase.ts
@@ -7,7 +7,10 @@ type Error = {
 const connectDatabase = async (database: string) => {
   try {
     const conn = await mongoose.connect(
-      (process.env.MONGODB_URI as string) + database
+      (process.env.MONGODB_URI as string) + database,
+      {
+        writeConcern: { w: "majority" },
+      }
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
